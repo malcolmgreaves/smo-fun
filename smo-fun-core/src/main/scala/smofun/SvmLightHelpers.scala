016c3cffe1e76100b025f85f950c2fbfa092391c
Malcolm Greaves <greaves.malcolm@gmail.com>
2016-07-25 12:35:44 -0700

    have model read OK, need test; wip

--- smo-fun-core/src/main/scala/smofun/SvmLightHelpers.scala
+++ smo-fun-core/src/main/scala/smofun/SvmLightHelpers.scala
@@ -1,15 +1,9 @@
 package smofun
 
 import java.io.{ BufferedWriter, File, FileWriter }
-import java.util.concurrent.TimeUnit
-
 import breeze.linalg.{ DenseVector, SparseVector }
-import spire.syntax.cfor._
-
-import scala.concurrent.duration.Duration
 import scala.io.Source
 import scala.language.postfixOps
-import scala.util.Random
 import scalaz.{ @@, Tag, \/ }
 
 object SvmLightHelpers {
@@ -22,6 +16,14 @@ object SvmLightHelpers {
   lazy val whitespaceSplit: String => Seq[String] =
     _.split("\\s+").toSeq
 
+  lazy val separateIndexValue: String => (Int, Double) =
+    bit => {
+      val sbits = bit.split(":")
+      val fIndex = sbits.head.toInt - 1
+      val fValue = sbits(1).toDouble
+      (fIndex, fValue)
+    }
+
   lazy val svmLightFmtSeparate: String => (Target, Seq[(Int, Double)]) =
     line => {
       val bits = whitespaceSplit(line)
@@ -29,13 +31,9 @@ object SvmLightHelpers {
 
       val fv =
         if (bits.length > 1) {
-          bits.slice(1, bits.length)
-            .map { b =>
-              val sbits = b.split(":")
-              val fIndex = sbits.head.toInt - 1
-              val fValue = sbits(1).toDouble
-              (fIndex, fValue)
-            }
+          bits
+            .slice(1, bits.length)
+            .map { separateIndexValue }
 
         } else
           Seq.empty
@@ -76,23 +74,74 @@ object SvmLightHelpers {
       }
     }
 
-  lazy val asSvmLightFmt: SvmDualModel => String = ???
+  lazy val asSvmLightFmt: SvmDualModel => Seq[String] =
+    svm => ???
 
-  lazy val fromSvmLightFmt: String => E[SvmDualModel] = ???
+  lazy val parseOut = (s: String) => s.split("#").head
+
+  lazy val parseSV: Int => String => (DenseVector[Double], Double, Double) =
+    dims => line => {
+
+      val bits = whitespaceSplit(line)
+
+      val (target, alpha) = {
+        val x = bits.head.toDouble
+        (
+          if (x < 0.0) -1.0 else 1.0,
+          math.abs(x)
+        )
+      }
+
+      val dv = DenseVector.zeros[Double](dims)
+      bits.slice(1, bits.length).foreach { b =>
+        val (i, v) = separateIndexValue(b)
+        dv(i) = v
+      }
+
+      (dv, target, alpha)
+    }
+
+  lazy val fromSvmLightFmt: Seq[String] => E[SvmDualModel] =
+    lines => \/.fromTryCatchNonFatal {
+      // ignore lines(0)
+      // ignore lines(1-2) TODO handle more kernels
+      val sigma = parseOut(lines(3)).toDouble
+      // ignore lines(4-6) TODO handle more kernels
+      val dimensionality = parseOut(lines(7)).toInt
+      // ignore lines(8)
+      val nSvs = parseOut(lines(9)).toInt - 1
+      val b = parseOut(lines(10)).toDouble
+      // now onto the support vectors!
+      val (svs, alphas, targets) =
+        lines
+          .slice(11, lines.size)
+          .map { line => parseSV(dimensionality)(parseOut(line)) }
+          .unzip3
+
+      SvmDualModel(
+        alphas = alphas.toIndexedSeq,
+        targets = targets.toIndexedSeq,
+        vectors = svs.toIndexedSeq,
+        b = b,
+        K = SmoHelpers.Kernels.gaussian(sigma)
+      )
+    }
 
   type E[T] = \/[Throwable, T]
 
   lazy val writeModel: SvmDualModel => File => E[Unit] =
     svm => fi => \/.fromTryCatchNonFatal {
       val w = new BufferedWriter(new FileWriter(fi))
-      val s = asSvmLightFmt(svm)
-      w.write(s)
+      asSvmLightFmt(svm).foreach { line =>
+        w.write(line)
+        w.newLine()
+      }
       w.close()
     }
 
   lazy val readModel: File => E[SvmDualModel] =
     fi => \/.fromTryCatchNonFatal {
-      Source.fromFile(fi).getLines().mkString("\n")
+      Source.fromFile(fi).getLines().toSeq
     }.flatMap { fromSvmLightFmt }
 
 }
