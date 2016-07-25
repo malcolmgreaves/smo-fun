b0d0b6061b8a34427e7147cf1f882b13d5a0b6e5
Malcolm Greaves <malcolmgreaves@users.noreply.github.com>
2016-07-25 11:38:47 -0700

    added visualization of +/- classifications (#2)
    * TrainVisualizeHyperplaneM does;
      * trains an SVM on labeled data
      * re-predicts on _same_ data
      * plots predicts vs actual

--- smo-fun-cmd/src/main/scala/smofun/PerfEvalSmoM.scala
+++ smo-fun-cmd/src/main/scala/smofun/PerfEvalSmoM.scala
@@ -12,85 +12,17 @@ import scala.util.{ Random, Try }
 object PerfEvalSmoM extends App {
 
   import SvmLightHelpers._
+  import AppHelpers._
 
   lazy val smoSolver = SequentialMinimalOptimization.train(conf) _
 
-  def shuffle[T](xs: Seq[T]): Seq[T] =
-    xs
-      .map { x => (x, Random.nextInt()) }
-      .sortBy { _._2 }
-      .map { _._1 }
-
-  lazy val splitPosNeg = (xs: Seq[(Vec, Target)]) =>
-    (
-      xs.filter { _._2 > 0.0 },
-      xs.filter { _._2 < 0.0 }
-    )
-
-  lazy val mkBalanced: (Seq[(Vec, Target)], Seq[(Vec, Target)]) => Seq[(Vec, Target)] =
-    (pos, neg) =>
-      if (pos.size < neg.size)
-        pos ++ neg.slice(0, pos.size)
-      else
-        pos.slice(0, neg.size) ++ neg
-
-  lazy val splitProp = (prop: Double) => (xs: Seq[(Vec, Target)]) => {
-    val splitIndx = (xs.size * prop).round.toInt
-    (
-      xs.slice(0, splitIndx),
-      xs.slice(splitIndx, xs.size)
-    )
-  }
-
-  lazy val splitTrainTest = (prop: Double) => {
-    val sp = splitProp(prop)
-    (xs: Seq[(Vec, Target)]) => {
-
-      val (pos, neg) = splitPosNeg(xs)
-      val (trainPos, testPos) = sp(pos)
-      val (trainNeg, testNeg) = sp(neg)
-      (
-        trainPos ++ trainNeg,
-        testPos ++ testNeg
-      )
-    }
-  }
-
-  case class ConfusionMatrix(tp: Int, fp: Int, tn: Int, fn: Int)
-  object ConfusionMatrix {
-    val zero = ConfusionMatrix(0, 0, 0, 0)
-  }
-
-  lazy val calcPerf: ConfusionMatrix => (Double, Double, Double) =
-    cm => {
-      import cm._
-
-      val (precision, recall) = {
-        val tpD = tp.toDouble
-        (
-          if (tp == 0 && fp == 0) 0.0 else tpD / (tpD + fp),
-          if (tp == 0 && tn == 0) 0.0 else tpD / (tpD + tn)
-        )
-      }
-      val f1 =
-        if (tp == 0 && (fp == 0 || fn == 0))
-          0.0
-        else
-          (2.0 * precision * recall) / (precision + recall)
-
-      (precision, recall, f1)
-    }
-
   /////////////////////////////////////////////////////////////////////////////
 
-  val loc = new File(Try(args.head).getOrElse("./data/diabetes"))
-  val doBalanced = Try(args(1).toBoolean).getOrElse(true)
-  val trainProp = Try(args(2).toDouble)
-    .toOption
-    .flatMap { x => if (x > 0.0 && x < 1.0) Some(x) else None }
-    .getOrElse(0.75)
-  val doLowMemUse = Try(args(3).toBoolean).getOrElse(false)
-  val doFullAlphaSearch = Try(args(4).toBoolean).getOrElse(false)
+  val loc = new File("data/SupportVectorMachineWithGaussianKernel_svmlight")
+  val doBalanced = false
+  val trainProp = 0.75
+  val doLowMemUse = true
+  val doFullAlphaSearch = true
   val c = Try(args(5).toDouble).getOrElse(1.0)
   val tol = Try(args(6).toDouble).getOrElse(0.001)
   val sigma = Try(args(7).toDouble).getOrElse(0.5)
@@ -116,9 +48,9 @@ object PerfEvalSmoM extends App {
   ////////
 
   val dimensionality = calculateDimensionality(loc)
-  val parse = parseSvmLightFmt(dimensionality)
 
   val (train, test) = {
+    val parse = parseSvmLightFmt(dimensionality)
     val data: Seq[(Vec, Target)] =
       Source
         .fromFile(loc)
@@ -144,21 +76,20 @@ object PerfEvalSmoM extends App {
         balanced
 
       } else {
-        println("Not doing any class rebalancing")
+        println("Not doing any class re-balancing")
         shuffled
       }
 
     splitTrainTest(trainProp)(finalLabeledData)
   }
 
-  println(
-    s"""Training on ${train.size} vectors, each of length $dimensionality
-        |Using the following SVM training configuration:
-        |$conf
-     """.stripMargin
-  )
-
   val svm = {
+    println(
+      s"""Training on ${train.size} vectors, each of length $dimensionality
+          |Using the following SVM training configuration:
+          |$conf
+     """.stripMargin
+    )
     val (svmModel, trainTime) = time { smoSolver(train) }
     println(
       s"""Finished training in ${trainTime.toSeconds} seconds.
@@ -181,7 +112,6 @@ object PerfEvalSmoM extends App {
             val prediction = pTar > 0.0
             //            val prediction = classifier(input)
             val targetIsTrue = target > 0.0
-            println(s"\tprediction: $pTar  target: $target")
             (targetIsTrue, prediction) match {
               case (true, true) => cm.copy(tp = cm.tp + 1)
               case (true, false) => cm.copy(fn = cm.fn + 1)
---
+++ smo-fun-cmd/src/main/scala/smofun/TrainVisualizeHyperplaneM.scala
@@ -0,0 +1,192 @@
+package smofun
+
+import java.awt
+import java.io.File
+
+import breeze.linalg.{ DenseVector, SparseVector }
+import com.quantifind.charts.highcharts.{ Highchart, Series, SeriesType }
+import com.quantifind.charts.repl.IterablePair
+import smofun.SequentialMinimalOptimization._
+import smofun.SmoHelpers.Kernels._
+import smofun.SmoHelpers._
+
+import scala.io.Source
+import scala.language.postfixOps
+import scala.util.Try
+
+object TrainVisualizeHyperplaneM extends App {
+  import SvmLightHelpers._
+  import AppHelpers._
+
+  /////////////////////////////////////////////////////////////////////////////
+
+  import SvmLightHelpers._
+  import AppHelpers._
+
+  /////////////////////////////////////////////////////////////////////////////
+
+  val loc = Try(new File(args(1))).getOrElse(new File("data/SupportVectorMachineWithGaussianKernel_svmlight"))
+  val doBalanced = false
+  val doLowMemUse = true
+  val doFullAlphaSearch = true
+  val c = 1.0
+  val tol = 0.001
+  val sigma = Try(args.head.toDouble).getOrElse(0.01)
+  println(
+    s"""Configuration
+        |Using labeled data from:      $loc
+        |Doing +/- balanced training?: $doBalanced
+        |Predict w/ low memory use?:   $doLowMemUse
+        |Doing Full Alpha_2 Search?:   $doFullAlphaSearch
+        |C (cost parameter):           $c
+        |Tolerance for Alpha Change:   $tol
+        |S.D. of Gaussian Kernel:      $sigma
+     """.stripMargin
+  )
+  val smoSolver = SequentialMinimalOptimization.train(
+    SvmConfig(
+      C = c,
+      tolerance = tol,
+      K = gaussian(sigma),
+      doFullAlphaSearch = doFullAlphaSearch
+    )
+  ) _
+
+  ////////
+
+  val dimensionality = calculateDimensionality(loc)
+
+  val data = {
+    val parse = parseSvmLightFmt(dimensionality)
+    val origData: Seq[(Vec, Target)] =
+      Source
+        .fromFile(loc)
+        .getLines()
+        .map { parse }
+        .toSeq
+    val shuffled = shuffle(origData)
+    val (pos, neg) = splitPosNeg(shuffled)
+
+    println(s"${pos.size} + examples and ${neg.size} - examples")
+
+    val finalLabeledData =
+      if (doBalanced) {
+        val balanced = mkBalanced(pos, neg)
+        println(
+          s"""Doing + and - class balancing
+              |Balanced size: ${balanced.size}""".stripMargin
+        )
+        balanced
+
+      } else {
+        println("Not doing any class rebalancing")
+        shuffled
+      }
+
+    finalLabeledData
+  }
+
+  val svm = {
+    println(s"Training on ${data.size} vectors, each of length $dimensionality")
+    val (svmModel, trainTime) = time { smoSolver(data) }
+    println(
+      s"""Finished training in ${trainTime.toSeconds} seconds.
+          |Found ${svmModel.size} support vectors.
+     """.stripMargin
+    )
+    svmModel
+  }
+
+  /// Visualize Hyperplane
+
+  val classifier = svmClassifier(true)(svm)
+
+  val (colorPredPos, colorPredNeg) = (awt.Color.RED, awt.Color.BLUE)
+  val (colorActPos, colorActNeg) = (awt.Color.ORANGE, awt.Color.GREEN)
+
+  val xs = data.map { case (vec, _) => vec(0) }
+  val ys = data.map { case (vec, _) => vec(1) }
+
+  val everything = data.map {
+    case (vec, target) =>
+      (classifier(vec), target > 0.0, vec(0), vec(1))
+  }
+
+  val (predictedPos, predictedNeg) = {
+    val byPredict = everything.groupBy { _._1 }
+    (
+      byPredict.getOrElse(true, Seq.empty),
+      byPredict.getOrElse(false, Seq.empty)
+    )
+  }
+
+  val (actualPos, actualNeg) = {
+    val byActual = everything.groupBy { _._2 }
+    (
+      byActual.getOrElse(true, Seq.empty),
+      byActual.getOrElse(false, Seq.empty)
+    )
+  }
+
+  import com.quantifind.charts.Highcharts._
+  import com.quantifind.charts.highcharts
+
+  lazy val splitXY: Seq[(_, _, Double, Double)] => (Seq[Double], Seq[Double]) =
+    values => (values.map { _._3 }, values.map { _._4 })
+
+  def better_scatter(
+    x: Seq[Double],
+    y: Seq[Double],
+    color: awt.Color
+  ): Highchart = {
+
+    import Highchart._
+    plot(
+      Highchart(
+        Series(
+          data = x.zip(y).toSeq,
+          chart = Some(SeriesType.scatter),
+          color = Some(highcharts.Color.javaColorToHex(color))
+        )
+      )
+    )
+  }
+
+  val sp1 = {
+    val (x, y) = splitXY(predictedPos)
+    better_scatter(x, y, colorPredPos)
+  }
+
+  hold()
+
+  val sp2 = {
+    val (x, y) = splitXY(predictedNeg)
+    better_scatter(x, y, colorPredNeg)
+  }
+
+  title("Predicted")
+  yAxis("Feature Value #1")
+  xAxis("Feature Value #2")
+
+  legend(Seq("RED is predicted +", "BLUE is predicted -"))
+
+  unhold()
+
+  val sp3 = {
+    val (x, y) = splitXY(actualPos)
+    better_scatter(x, y, colorActPos)
+  }
+
+  hold()
+
+  val sp4 = {
+    val (x, y) = splitXY(actualNeg)
+    better_scatter(x, y, colorActNeg)
+  }
+
+  title("Actual")
+  yAxis("Feature Value #1")
+  xAxis("Feature Value #2")
+  legend(Seq("ORANGE is actual +", "GREEN is actual -"))
+
+}
